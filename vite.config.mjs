d275722079943a247f387147fbb21401ed9aa2e1
Matthias Hryniszak <padcom@gmail.com>
2024-05-03 22:55:21 +0200

    Moved over to shared vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,65 +1,15 @@
 /* eslint-env node */
-import { fileURLToPath } from 'url'
-import { defineConfig } from 'vitest/config'
-
-import svg from 'vite-svg-loader'
-import vue from '@vitejs/plugin-vue'
-import i18n from '@padcom/vite-plugin-vue-i18n'
-import eslint from 'vite-plugin-eslint'
+import { defineVueAppConfig } from '@padcom/vite-config-vue'
 import nesting from 'tailwindcss/nesting'
 import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-  define: {
-    // Enable / disable Options API support
-    __VUE_OPTIONS_API__: true,
-    // Enable / disable devtools support in production
-    __VUE_PROD_DEVTOOLS__: false,
-    // Enable / disable detailed warnings for hydration mismatches in production
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-  },
-  plugins: [
-    svg({
-      defaultImport: 'component',
-    }),
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => tag.includes('-'),
-        },
-      },
-    }),
-    i18n({
-      runtimeOnly: false,
-      include: fileURLToPath(new URL('./src/i18n/locales/**', import.meta.url)),
-    }),
-    eslint({
-      lintOnStart: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+export default defineVueAppConfig({
   css: {
     postcss: {
       plugins: [
         nesting(),
         tailwindcss(),
-        autoprefixer(),
       ],
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: [
-      './vitest.setup.js',
-    ],
-    coverage: {
-      enabled: true,
-      reporter: ['text', 'lcov'],
     },
   },
 })
